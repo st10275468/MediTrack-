4ad84b04f1c692d26935d37fd6e33feea278225b
st10275468 <st10275468@vcconnect.edu.za>
2025-10-01 15:44:00 +0200

    Completed Tab menu functionality on Search and Reminders activity

--- app/src/main/java/com/example/meditrack/ReminderActivity.kt
+++ app/src/main/java/com/example/meditrack/ReminderActivity.kt
@@ -1,11 +1,35 @@
 package com.example.meditrack
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.tabs.TabLayout
 
 class ReminderActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_reminders)
+
+        //Tab Menu functionality
+        val tabMenu = findViewById<TabLayout>(R.id.TabMenu)
+        tabMenu.getTabAt(3)?.select()
+        tabMenu.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener{
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+                when (tab?.position) {
+                    0 -> {val intent = Intent(this@ReminderActivity, DashboardActivity::class.java)
+                        startActivity(intent)}
+                    1 -> { val intent = Intent(this@ReminderActivity, SearchActivity::class.java)
+                        startActivity(intent)}
+                    2 -> {
+                        val intent = Intent(this@ReminderActivity, ProfileActivity::class.java)
+                        startActivity(intent)
+                    }
+                    3 -> {}
+                }
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab?) {}
+            override fun onTabReselected(tab: TabLayout.Tab?) {}
+        })
     }
 }
--- app/src/main/java/com/example/meditrack/SearchActivity.kt
+++ app/src/main/java/com/example/meditrack/SearchActivity.kt
@@ -1,11 +1,44 @@
 package com.example.meditrack
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.tabs.TabLayout
 
 class SearchActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
+
+        //Tab Menu functionality
+        val tabMenu = findViewById<TabLayout>(R.id.TabMenu)
+        tabMenu.getTabAt(1)?.select()
+        tabMenu.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener{
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+                when (tab?.position) {
+                    0 -> {val intent = Intent(this@SearchActivity, DashboardActivity::class.java)
+                        startActivity(intent)}
+                    1 -> {}
+                    2 -> {
+                        val intent = Intent(this@SearchActivity, ProfileActivity::class.java)
+                        startActivity(intent)
+                    }
+                    3 -> {
+                        val intent = Intent(this@SearchActivity, ReminderActivity::class.java)
+                        startActivity(intent)
+                    }
+                }
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab?) {}
+            override fun onTabReselected(tab: TabLayout.Tab?) {}
+        })
+
     }
+
+
+
+
+
+
 }
