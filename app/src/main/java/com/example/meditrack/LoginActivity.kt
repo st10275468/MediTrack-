ba94dc849fa8c7ddd0cde9a023cb223d5047bfb0
st10275468 <st10275468@vcconnect.edu.za>
2025-10-01 12:06:33 +0200

    Made the Register Text Link functional

--- app/src/main/java/com/example/meditrack/LoginActivity.kt
+++ app/src/main/java/com/example/meditrack/LoginActivity.kt
@@ -1,11 +1,22 @@
 package com.example.meditrack
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        //Text link to take the user to the register view
+        val txtRegister = findViewById<TextView>(R.id.txtRegister)
+            txtRegister.setOnClickListener{
+            val intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
+
+        }
+
     }
 }
