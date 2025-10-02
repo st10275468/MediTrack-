2e80faab599fcd6d436e1255f09d0689accc9b8e
James Knox <jamesknox361@gmail.com>
2025-10-02 11:04:20 +0200

    Added registration functionality, adds user to FirebaseAuth and Firestore

--- app/src/main/java/com/example/meditrack/RegisterActivity.kt
+++ app/src/main/java/com/example/meditrack/RegisterActivity.kt
@@ -1,11 +1,79 @@
 package com.example.meditrack
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+
+import com.google.firebase.firestore.FirebaseFirestore
 
 class RegisterActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+    private lateinit var db: FirebaseFirestore
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+
+        auth = FirebaseAuth.getInstance()
+        db = FirebaseFirestore.getInstance()
+
+        val emailInput = findViewById<EditText>(R.id.email)
+        val passwordInput = findViewById<EditText>(R.id.password)
+        val passwordConfirmInput = findViewById<EditText>(R.id.confirmPassword)
+        val registerBtn = findViewById<Button>(R.id.btnRegister)
+
+        registerBtn.setOnClickListener {
+            val email = emailInput.text.toString().trim()
+            val password = passwordInput.text.toString().trim()
+            val confirmPassword = passwordConfirmInput.text.toString().trim()
+
+            registerUser(email, password, confirmPassword)
+        }
+    }
+
+    private fun registerUser(email: String, password: String, confirmPassword: String){
+        if (email.isEmpty() || password.isEmpty() || confirmPassword.isEmpty()) {
+            Toast.makeText(this, "Please fill out all fields", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        if (password != confirmPassword){
+            Toast.makeText(this, "Passwords do not match", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener {
+            task ->
+            if (task.isSuccessful){
+                val user = auth.currentUser
+
+                saveUserToFirestore(user?.uid, email)
+
+                startActivity(Intent(this, LoginActivity::class.java))
+                finish()
+            } else {
+                Toast.makeText(this, "Error: ${task.exception?.message}", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun saveUserToFirestore(uid: String?, email: String) {
+        if (uid == null) return
+        val userMap = hashMapOf("email" to email, "createdAt" to System.currentTimeMillis())
+
+        db.collection("users").document(uid).set(userMap)
+            .addOnSuccessListener {
+                Toast.makeText(this, "Succesfully registered", Toast.LENGTH_SHORT).show()
+            }
+            .addOnFailureListener {
+                Toast.makeText(this, "Firestore error", Toast.LENGTH_SHORT).show()
+            }
+
     }
 }
